59e4f8c396116403bdc3ecb8b647c0d2c721eeb5
deja.craft <dejacraft28@gmail.com>
2023-03-09 23:06:12 -0500

    ReactMovie

--- src/App.js
+++ src/App.js
@@ -24,12 +24,33 @@ export default function App() {
     setMovie(data);
   };
 
+  //This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
   // USE OUR COMPONENTS IN APPs RETURNED JSX
   // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
-      <MovieDisplay />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
+
+const getMovie = async (searchTerm) => {
+	// make fetch request and store response
+	try {
+		const response = await fetch(
+			`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+		);
+		// Parse JSON response into a javascript object
+		const data = await response.json();
+		//set the Movie state to the movie
+		setMovie(data);
+	} catch(e){
+		console.error(e)
+	}
+};
